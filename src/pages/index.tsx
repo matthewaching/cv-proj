4829f0ee5c8dc7e37d1e37b90cf38e6e94740511
Matthew Ching <matthewaching@gmail.com>
2022-09-14 20:43:21 -0500

    Remove boilerplate elements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,77 +14,30 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <header className="-mb-20">
+        <nav className="p-8">
+          <ul className="flex gap-8">
+            <li className="ml-auto">Home</li>
+            <li>Portfolio</li>
+            <li>About</li>
+          </ul>
+        </nav>
+      </header>
+
       <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
         <TypeIt className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
-          Hi! My name is <span className="text-purple-300">Matthew Ching</span>,<br/>
-          and I am a software developer. <br/>Thanks for stopping by.
+          Hi! My name is <span className="text-purple-300">Matt Ching</span>,
+          <br />
+          and I am a software developer. <br />
+          Thanks for stopping by.
         </TypeIt>
-        <p className="text-2xl text-gray-700">This stack uses:</p>
-        <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3">
-          <TechnologyCard
-            name="NextJS"
-            description="The React framework for production"
-            documentation="https://nextjs.org/"
-          />
-          <TechnologyCard
-            name="TypeScript"
-            description="Strongly typed programming language that builds on JavaScript, giving you better tooling at any scale"
-            documentation="https://www.typescriptlang.org/"
-          />
-          <TechnologyCard
-            name="TailwindCSS"
-            description="Rapidly build modern websites without ever leaving your HTML"
-            documentation="https://tailwindcss.com/"
-          />
-          <TechnologyCard
-            name="tRPC"
-            description="End-to-end typesafe APIs made easy"
-            documentation="https://trpc.io/"
-          />
-          <TechnologyCard
-            name="Next-Auth"
-            description="Authentication for Next.js"
-            documentation="https://next-auth.js.org/"
-          />
-          <TechnologyCard
-            name="Prisma"
-            description="Build data-driven JavaScript & TypeScript apps in less time"
-            documentation="https://www.prisma.io/docs/"
-          />
-        </div>
-        <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
-          {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
-        </div>
       </main>
+
+      <section className="container min-h-screen">
+        <h1>Portfolio Section</h1>
+      </section>
     </>
   );
 };
 
 export default Home;
-
-type TechnologyCardProps = {
-  name: string;
-  description: string;
-  documentation: string;
-};
-
-const TechnologyCard = ({
-  name,
-  description,
-  documentation,
-}: TechnologyCardProps) => {
-  return (
-    <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
-      <a
-        className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
-        href={documentation}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Documentation
-      </a>
-    </section>
-  );
-};
